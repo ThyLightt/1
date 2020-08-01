16c487f12781754a8ede668db416ef27efcb8c45
ThyLightt <izengard96@gmail.com>
2020-08-01 11:26:03 +0300

    some task

--- lab2/main.js
+++ lab2/main.js
@@ -1,47 +1,48 @@
-let submButton = document.getElementById("submitRegistration");
-submButton.onclick = function () {
+// let a = +prompt("enter a value");
+// let b = +prompt("enter b value");
+// let sum = 0;
+// let qu = 0;
+//
+// for(let i=a; i<b; i++) {
+//     if (i%2 === 0){
+//         sum+=i;
+//     }
+//     else {
+//         qu++;
+//     }
+// }
+// console.dir("qu: " + qu + "sum: " + sum);
 
-    let emailAdd = document.getElementById("email");
-    validateEmail(emailAdd);
-    let usernameAdd = document.getElementById("username");
-    validateUsername(usernameAdd);
-    let passwordAdd = document.getElementById("password");
-    validatePassword(passwordAdd);
-    let passwordConfAdd = document.getElementById("passwordConf");
-    validatePasswordConf(passwordConfAdd);
-};
+//
 
-function validateEmail (email) {
-   let emailPattern =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-   let result = emailPattern.test(email.value);
-   if(result === true) {
-       email.style.border = "2px solid green"
-   }else {
-       email.style.border = "2px solid red"
-   }
+// for (let j = 999; j<10000; j++ ) {
+//     let string = j.toString();
+//     let sum = 0;
+//     let dob = 0;
+//     for (let i = 0; i<string.length;i++){
+//         sum += +string[i];
+//         dob += +string[i];
+//     }
+//     if (sum === dob) {
+//         alert("qweqwe " + j);
+//     }
+// }
+
+//
+
+let a = +prompt("a 1 sq");
+let b = +prompt("a 1 sq");
+
+let c = +prompt("a 2 sq");
+let d = +prompt("a 2 sq");
+if (area(a,b)>area(c,d)){
+    alert("1st is bigger")
+}else {
+    alert("2nd is bigger")
 }
-function validateUsername(username) {
-    let usernamePattern = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
-    let result = usernamePattern.test(username.value);
-    if(result === true) {
-        username.style.border = "2px solid green"
-    }else {
-        username.style.border = "2px solid red"
-    }
-}
-function validatePassword (password) {
-    let passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    let result = passwordPattern.test(password.value);
-    if (result === true) {
-        password.style.border = "2px solid green"
-    } else {
-        password.style.border = "2px solid red"
-    }
-}
-function validatePasswordConf(passwordConf) {
-    if ( passwordConf.value === document.getElementById("password").value) {
-        passwordConf.style.border = "2px solid green"
-    } else {
-        passwordConf.style.border = "2px solid red";
-    }
+
+
+function area(a,b) {
+    return a*b;
+
 }
